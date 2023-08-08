06ee3bbe45d9bca3a082c1388e08632ccacfc77a
Vishal <vr15102002@gmail.com>
2023-08-08 18:32:29 +0530

    added some pages like write blog and login page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Home from "./pages/home/Home";
 import TopBar from "./Components/topbar/TopBar";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+import Settings from "./pages/setting/Settings";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
     <div>
       <TopBar/>
-      <Write/>
+      <Login/>
     </div>
     
   );
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,19 @@
+import "./login.css"
+
+export default function Login() {
+    return(
+        <div className="login">
+            <span className="loginTitle">Login</span>
+            <form className="loginForm">
+                <label>Email</label>
+                <input type="text" placeholder="Enter Your Email.."/>
+                <label>Password</label>
+                <input type="password" placeholder="Enter Your Password.."/>
+
+                <button className="loginButton">Login</button>
+                
+            </form>
+            <button className="loginRegisterButton">Register</button>
+        </div>
+    )
+}
